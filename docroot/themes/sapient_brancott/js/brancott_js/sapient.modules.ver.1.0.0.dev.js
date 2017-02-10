f50751f4327f170e85bab07ea3382e991e95f793
Rohit Pandey 3 <rpan22@sapient.com>
2017-02-10 13:12:28 +0530

    misc

--- docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
+++ docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
@@ -143,11 +143,29 @@ var heroObj = (function($, window, sapient) {
 					scrollTop: heightHero
 				}, 1000);
 			});
+		},
+		disableHeroHeightChangeonMobile = function() {
+			var windowHeight = $(window).height();
+			if (windowWidth > 1200 /*&& windowWidth < 1400*/) {
+				var right = (windowWidth - 1170) / 2;
+				$("footer .select-wrapper").css('right', right + 15 + 'px');
+			} /*else if (windowWidth > 1400) {
+				var right = (windowWidth - 1400) / 2;
+				$("footer .select-wrapper").css('right', right + 15 + 'px');
+			}*/
+		},
+
+		onResize = function() {
+			$(window).on('resize', function () {
+				debounce(sapient.hero.disableHeroHeightChangeonMobile,500,"testing HeroHeightChangeonMobile");
+				/*sapient.footer.setFooterDdownPos();*/
+			});
 		};
 
 		return {
 			// public + private states and behaviors
-			getHeightHero: getHeightHero
+			getHeightHero: getHeightHero,
+			disableHeroHeightChangeonMobile: disableHeroHeightChangeonMobile
 		};
 	}
 
