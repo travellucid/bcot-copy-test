5e16f0d84bfa057b52bcf2e6cc6b71cb223fb115
Gaurav Pahuja <gpahuja@sapient.com>
2017-02-14 07:39:31 +0530

    click to buy module commits

---
+++ docroot/modules/custom/pr_ctbuy_connector/src/Controller/RemotekeyAutocompleteController.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\pr_ctbuy_connector\Controller\RemotekeyAutocompleteController.
+ */
+
+namespace Drupal\pr_ctbuy_connector\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Class RemotekeyAutocompleteController.
+ *
+ * @package Drupal\pr_ctbuy_connector\Controller
+ */
+class RemotekeyAutocompleteController extends ControllerBase {
+
+  /**
+   * Request stack.
+   *
+   * @var RequestStack
+   */
+  public $request;
+
+  /**
+   * Class constructor.
+   *
+   * @param RequestStack $request
+   *   Request stack.
+   */
+  public function __construct(RequestStack $request) {
+    $this->request = $request;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+        // Load the service required to construct this class.
+        $container->get('request_stack')
+    );
+  }
+
+  /**
+   * Autocomplete.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function autocomplete() {
+    $string = $this->request->getCurrentRequest()->get('q');
+    $data = array();
+
+    // All the checks have already been done to this point, we can directly query
+    // the console.
+    $endpoint_url = \Drupal::config('pr_ctbuy_connector.settings')->get('pr_ctbuy_connector_endpoint');
+    $api_key = \Drupal::config('pr_ctbuy_connector.settings')->get('pr_ctbuy_connector_key');
+    $instance = \Drupal::config('pr_ctbuy_connector.settings')->get('pr_ctbuy_connector_instance');
+    $use_curl = \Drupal::config('pr_ctbuy_connector.settings')->get('pr_ctbuy_connector_use_curl');
+
+
+    $endpoint_url .= '/GetListForInstance/?instance=' . $instance . '&search=' . $string;
+
+    if ($string) {
+      //print $endpoint_url; exit;
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $endpoint_url);
+      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('api_key: ' . $api_key));
+      curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      // Bypass SSL checking.
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+      $data = curl_exec($ch);
+      
+      $error = curl_error($ch);
+
+      curl_close($ch);
+    }
+    else {
+//      $options = array(
+//        'method' => 'GET',
+//        'timeout' => 15,
+//        'headers' => array(
+//          'api_key' => $api_key,
+//        ),
+//      );
+//
+//      try {
+//        $response = \Drupal::httpClient()->get($endpoint_url, array('headers' => array('Accept' => 'text/plain')));
+//        $data = (string) $response->getBody();
+//        if (empty($data)) {
+//          return FALSE;
+//        }
+//      }
+//      catch (RequestException $e) {
+//        return FALSE;
+//      }
+//
+//      $data = $result->data;
+    }
+    //$data = str_replace("world","Peter","Hello world!");
+    //$data[] = ['value' => 'termvalue', 'label' => 'termname'];
+    print_r($data); exit;
+    return JsonResponse::create($data);
+  }
+
+}
--- docroot/modules/custom/pr_ctbuy_connector/src/Plugin/Field/FieldWidget/PrCtbuyConnectorWidget.php
+++ docroot/modules/custom/pr_ctbuy_connector/src/Plugin/Field/FieldWidget/PrCtbuyConnectorWidget.php
@@ -5,6 +5,7 @@ namespace Drupal\pr_ctbuy_connector\Plugin\Field\FieldWidget;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\ConfigFormBase;
 
 /**
  * Plugin implementation of the 'pr_ctbuy_connector' widget.
@@ -18,22 +19,48 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
- 
- class PrCtbuyConnectorWidget extends WidgetBase {
+class PrCtbuyConnectorWidget extends WidgetBase {
 
   /**
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $endpoint_url = \Drupal::config('pr_ctbuy_connector.settings')->get('pr_ctbuy_connector_endpoint');
+    $api_key = \Drupal::config('pr_ctbuy_connector.settings')->get('pr_ctbuy_connector_key');
+    //print "here" . $endpoint_url; exit;
+    //$element['#markup'] = t("The click to buy Module is not properly configured, please check it" . $endpoint_url);
+    // Exit if not configured
+    if ($endpoint_url === NULL || $api_key === NULL) {
+      $element['#markup'] = t("The click to buy Module is not properly configured, please check it here: !url", array('!url' => 'admin/config/prctbuyconnector'));
+      return $element;
+    }
+
+
     $value = isset($items[$delta]->value) ? $items[$delta]->value : '';
+    // Construct the widget
     $element += array(
+      '#type' => 'fieldset',
+    );
+    $required = $element['#required'];
+  
+    $element['remote_key'] = array(
+      '#title' => t('Click to buy Console, Remote Key'),
       '#type' => 'textfield',
-      '#default_value' => $value,
-      '#size' => 7,
-      '#maxlength' => 7,
-      '#element_validate' => array(
-        array($this, 'validate'),
-      ),
+      '#required' => $required,
+      '#autocomplete_route_name' => 'pr_ctbuy_connector.remote_key_autocomplete',
+      '#autocomplete_route_parameters' => array(),
+      // use #default_value to prepopulate the element
+      // with the current saved value
+      '#default_value' => isset($item['remote_key']) ? $item['remote_key'] : '',
+    );
+
+    $element['title'] = array(
+      '#title' => t('Click to buy Console, text of the CTA'),
+      '#type' => 'textfield',
+      '#required' => $required,
+      // use #default_value to prepopulate the element
+      // with the current saved value
+      '#default_value' => isset($item['title']) ? $item['title'] : '',
     );
     return array('value' => $element);
   }
