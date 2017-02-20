da3b019e6b218db435054b8443385227311fdeb3
Nitin Suri <nsuri@sapient.com>
2017-02-20 12:39:10 +0530

    adding the our wines page

--- docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
+++ docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
@@ -529,6 +529,55 @@ sapient.followUs = followUsObj.getInstance();
 
 sapient.followUs.setContentWidth();
 sapient.followUs.onResize();
+var ourWines = (function($, window, sapient) {
+
+	var filterWinesCollection;
+
+	function createFilterWinesCollection() {
+		var filterWines = function() {
+			var allProductsGrid = $("#response-wrapper").html();
+			$(".filter-item").on("click",function(e){
+				e.preventDefault();
+
+				if($(this).data("categoryFilter") === "all-data") {
+
+					$("#response-wrapper").html(allProductsGrid);
+
+				} else {
+
+					var wineType = $(this).data("categoryFilter");
+					$.ajax({
+						url: "/search-page?range=" + wineType,
+						type: "GET",
+						success: function (data) {
+							$("#response-wrapper").html(data);
+						}
+					});
+				}
+			});
+		};
+
+
+		return {
+			// public + private states and behaviors
+			filterWines: filterWines
+		};
+	}
+
+	return {
+		getInstance: function() {
+			if (!filterWinesCollection) {
+				filterWinesCollection = createFilterWinesCollection();
+			}
+			return filterWinesCollection;
+		}
+	};
+
+})(jQuery, window, sapient);
+
+sapient.winesFilter = ourWines.getInstance();
+
+sapient.winesFilter.filterWines();
 var footerObj = (function($, window, sapient) {
 
 	var footerInstance;
