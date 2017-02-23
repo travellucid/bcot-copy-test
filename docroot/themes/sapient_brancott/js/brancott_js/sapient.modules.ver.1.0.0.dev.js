0dd2e3fb86457377d6684b928f023a6918094eec
Ayush Krishnatray <akri11@sapient.com>
2017-02-23 21:20:03 +0530

    more validation on form

--- docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
+++ docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
@@ -599,87 +599,99 @@ var validationObj = (function($, window, sapient) {
 	function createValidtaionInstance() {
 
 		var validate = function() {
-				var $input = $(".enquire-form form .group input"),
-					$select = $(".enquire-form form .group select");
-				$(".enquire-form .submit-info .submit-btn").click(function() {
-					$("#errMsg .messages").html("");
-					var checked = $('#check').is(':checked')
-						inputflag = 0,
-						inputarr =[]
-						selectflag = 0,
-						selectarr=[],
-						msgarr = [];
-					
-					$.each($input,function(index){
-						
-						if($($input[index]).val().length == 0) {
-							
-							$($(".enquire-form form .group label")[index]).addClass("error");
-							$($input[index]).addClass("error-border");
-							msgarr.push($($(".enquire-form form .group label")[index]).html());
-							
-						}
-						else {
-							
-							$($(".enquire-form form .group label")[index]).removeClass("error");
-							$($input[index]).removeClass("error-border");
-						}
-						inputarr.push($($input[index]).val().length);
+			var $input = $(".enquire-form form .group input"),
+				$select = $(".enquire-form form .group select");
 
-					})
+			$input.focusout(function(){
+				//console.log($(this).siblings('label'));
+				if($(this).val().length !== 0) {
+					$(this).siblings('label').addClass("text-entered")
+				}
+				else{
+					$(this).siblings('label').removeClass("text-entered")
+				}
+			})
+			
+			$(".enquire-form .submit-info .submit-btn").click(function() {
+				$("#errMsg .messages").html("");
+				var checked = $('#check').is(':checked')
+				inputflag = 0,
+					inputarr = []
+				selectflag = 0,
+					selectarr = [],
+					msgarr = [];
 
-					$.each($select,function(index){
-						
-						if($select[index].value == "") {
-							
-							$($select[index]).addClass("error-border");
-						}
-						else {
-							
-							$($select[index]).removeClass("error-border");
-						}
+				$.each($input, function(index) {
 
-						selectarr.push($select[index].value);
-					})
-					
-					if(msgarr.length != 0) {
-						
-						$("#errMsg").addClass("error");
-						$("#errMsg").css('display','block');
-						
-						$.each(msgarr , function(index){
-							
-							$("#errMsg .messages").append( '<span class="msg">' + msgarr[index] + '</span>');
-						})
-					}	
-						
-					$.each(inputarr,function(index){
-						
-						if(inputarr[index]===0){
-							
-							inputflag = 0;
-							console.log("false input")
-						}
-					})
-					
-					$.each(selectarr,function(index){
-						
-						if(selectarr[index] == ""){
-							selectflag = 0;
-							console.log("false select")
-						}
-					})
-					
-					if(!checked){
-						console.log("return false")
+					if ($($input[index]).val().length == 0) {
+
+						$($(".enquire-form form .group label")[index]).addClass("error");
+						$($input[index]).addClass("error-border");
+						msgarr.push($($(".enquire-form form .group label")[index]).html());
+
+					} else {
+
+						$($(".enquire-form form .group label")[index]).removeClass("error");
+						$($input[index]).removeClass("error-border");
 					}
-					
-					else {
-						console.log("submit")
+					inputarr.push($($input[index]).val().length);
+
+				})
+
+				$.each($select, function(index) {
+
+					if ($select[index].value == "") {
+
+						$($select[index]).addClass("error-border");
+						msgarr.push($($(".enquire-form form .group select option[value='']")[index]).text());
+					} else {
+
+						$($select[index]).removeClass("error-border");
 					}
 
-				});
-			}
+					selectarr.push($select[index].value);
+				})
+
+				if (msgarr.length != 0) {
+
+					$("#errMsg").addClass("error");
+					$("#errMsg").css('display', 'block');
+
+					$.each(msgarr, function(index) {
+
+						$("#errMsg .messages").append('<span class="msg">' + msgarr[index] + '</span>');
+					})
+				}
+
+				$.each(inputarr, function(index) {
+
+					if (inputarr[index] === 0) {
+
+						inputflag = 0;
+						console.log("false input")
+						return false;
+					}
+				})
+
+				$.each(selectarr, function(index) {
+
+					if (selectarr[index] == "") {
+						selectflag = 0;
+						console.log("false select")
+						return false;
+					}
+				})
+
+				if (!checked) {
+					console.log("return false")
+					return false;
+				} else {
+					console.log("submit")
+					return true;
+				}
+
+			});
+		}
 		return {
 			// public + private states and behaviors
 			validate: validate
@@ -694,7 +706,7 @@ var validationObj = (function($, window, sapient) {
 			return validationInstance;
 		}
 	};
-	
+
 
 })(jQuery, window, sapient);
 
