c0ce64f666d60d5ad7e9b250d4333c04e5490882
Nitin Suri <nsuri@sapient.com>
2017-03-03 16:17:55 +0530

    menus

--- docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
+++ docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
@@ -395,7 +395,8 @@ var commonObj = (function($, window, sapient) {
 			toggleAwardsDetails: toggleAwardsDetails,
 			addBgNoise: addBgNoise,
 			/*emptyform:emptyform,*/
-			assignTouchDeviceClass: assignTouchDeviceClass
+			assignTouchDeviceClass: assignTouchDeviceClass,
+			killHash: killHash
 		};
 	}
 
@@ -416,7 +417,7 @@ sapient.common.hideLinkText();
 sapient.common.addBgNoise();
 sapient.common.toggleAwardsDetails();
 sapient.common.assignTouchDeviceClass();
-/*sapient.common.emptyform();*/
+sapient.common.killHash();
 
 
 
