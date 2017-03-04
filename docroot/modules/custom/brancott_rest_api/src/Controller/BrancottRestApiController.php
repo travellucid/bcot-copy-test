ecf2ded1fa5231e522f07ec4cfaa59db316261dc
Gaurav Pahuja <gpahuja@sapient.com>
2017-03-04 10:18:41 +0530

    commented code removed

--- docroot/modules/custom/brancott_rest_api/src/Controller/BrancottRestApiController.php
+++ docroot/modules/custom/brancott_rest_api/src/Controller/BrancottRestApiController.php
@@ -18,39 +18,36 @@ class BrancottRestApiController extends ControllerBase {
    * Callback function to get the data from REST API
    */
   public function getProductDetails($wine_id, $locale = 'nz') {
-    
+
     $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
-	$lang_explode = explode("-", $language);
-	
-	if($language == 'en'){
-		$locale = 'row';
-	}
-	else{
-		$locale = $lang_explode[1];
-	}
-	//print_r($locale);die;
+    $lang_explode = explode("-", $language);
+
+    if ($language == 'en') {
+      $locale = 'row';
+    }
+    else {
+      $locale = $lang_explode[1];
+    }
     $data = array();
     $response = $data = $result = null;
     $request_type = 'wine_details';
     $cid = $request_type . '_' . $language . '_' . $wine_id;
     $cache = \Drupal::cache()->get($cid);
-    if (0) {
+    if ($cache) {
       $result = json_decode($cache->data);
       return $result;
     }
     else {
-	$response = brancott_rest_api_reponse('http://gateway.pernod-ricard-winemakers.com/v2/brancott%20estate/'.$locale.'/wines/'.$wine_id.'/en');
-     //$response = brancott_rest_api_reponse('http://brancottvanillascfanqpukk.devcloud.acquia-sites.com/sample.json');
-	 //print_r($response);exit;
+      $response = brancott_rest_api_reponse('http://gateway.pernod-ricard-winemakers.com/v2/brancott%20estate/' . $locale . '/wines/' . $wine_id . '/en');
     }
-	
+
     if ($response) {
       \Drupal::cache()->set($cid, $response);
       $result = json_decode($response);
       $data = array();
 
       // # add all the data in one multiple dim array
-    
+
       $data = $result;
       // display the content in the middle section of the page
     }
