1861099cf8b1d1799aacdb0f1e2f7a56d2899b1d
Ayush Krishnatray <akri11@sapient.com>
2017-03-06 12:57:35 +0530

    changes in padding

--- docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
+++ docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
@@ -396,7 +396,7 @@ var commonObj = (function($, window, sapient) {
 					isAndroid =navigator.userAgent.indexOf('Android') >=0;
 
 				if(isIOS || isAndroid) {
-					$("#find-us-component #map-overlay a[href^='tel']").addClass("")
+					$("#find-us-component #map-overlay a[href^='tel']").addClass("touchDevices")
 				}
 			},
 
