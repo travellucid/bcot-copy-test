eba376ea164f3bdecbc9b5ac395b3fd0f7653648
Krishan Chandra <kchan8@sapient.com>
2017-03-10 15:53:32 +0530

    404 error page

--- docroot/404.php
+++ docroot/404.php
@@ -1,14 +1,33 @@
-<div class="error-page">
-	
-      			<picture>
-				<source  media="(max-width: 321px)" srcset="/themes/sapient_brancott/css/images/wine-spill-graphic-JPG.jpg" class="background-image">   
-				<source  media="(max-width: 691px)" srcset="/themes/sapient_brancott/css/images/wine-spill-graphic-JPG.jpg" class="background-image">
-				<source  media="(max-width: 769px)" srcset="/themes/sapient_brancott/css/images/wine-spill-graphic-JPG.jpg" class="background-image">  
-				<source  media="(max-width: 1281px)" srcset="/themes/sapient_brancott/css/images/wine-spill-graphic-JPG.jpg" class="background-image"> 
-				<img src="/themes/sapient_brancott/css/images/wine-spill-graphic-JPG.jpg" alt="Error Page">
-			</picture>
+<!DOCTYPE html>
+<html  lang="en" dir="ltr" xmlns:article="http://ogp.me/ns/article#" xmlns:book="http://ogp.me/ns/book#" xmlns:product="http://ogp.me/ns/product#" xmlns:profile="http://ogp.me/ns/profile#" xmlns:video="http://ogp.me/ns/video#" prefix="content: http://purl.org/rss/1.0/modules/content/  dc: http://purl.org/dc/terms/  foaf: http://xmlns.com/foaf/0.1/  og: http://ogp.me/ns#  rdfs: http://www.w3.org/2000/01/rdf-schema#  schema: http://schema.org/  sioc: http://rdfs.org/sioc/ns#  sioct: http://rdfs.org/sioc/types#  skos: http://www.w3.org/2004/02/skos/core#  xsd: http://www.w3.org/2001/XMLSchema# ">
+  <head>
+    <meta charset="utf-8" />
+<meta name="title" content="404 | Brancott Estate" />
+<meta name="Generator" content="Drupal 8 (https://www.drupal.org)" />
+<meta name="MobileOptimized" content="width" />
+<meta name="HandheldFriendly" content="true" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>404 | Brancott Estate</title>
+<link rel="stylesheet" href="/themes/sapient_brancott/css/brancott_css/sapient.ver.1.0.0.css?omlfmq" media="all" />
+   
+<!--[if lte IE 8]>
+<script src="/core/assets/vendor/html5shiv/html5shiv.min.js?v=3.7.3"></script>
+<![endif]-->
+
+  </head>
+  <body class="path-node page-node-type-error-page has-glyphicons">
+  <div class="background-noise-section">
+ <div class="error-page background-noise-section">
 	<div class="error-info-wrapper">
-		<h1 class="error-heading">Oops!</h1>
-		<h2>This link may be broken or the page may have been removed.</h2>
+		<h1 class="error-heading">OOPS!</h1>
+		<h2>THIS LINK MAY BE BROKEN OR THE PAGE MAY HAVE BEEN REMOVED.</h2>
 	</div>
+	<img src="/themes/sapient_brancott/css/images/wine-spill-graphic-PNG.png" alt="Error" class="background-image">
 </div>	
+</div>
+  </body>
+</html>
+
+
+
+
