cc12c5ef395333b5795be708e8e802ab95bcf217
Gaurav Pahuja <gpahuja@sapient.com>
2017-03-21 12:33:03 +0530

    filter block changes

--- docroot/modules/custom/brancott_rest_api/src/Controller/BrancottRestApiController.php
+++ docroot/modules/custom/brancott_rest_api/src/Controller/BrancottRestApiController.php
@@ -21,10 +21,8 @@ class BrancottRestApiController extends ControllerBase {
 
     $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
     $lang_explode = explode("-", $language);
-    $cache_ttl = \Drupal::config('brancott_our_wines.settings')->get('cache_ttl'); 
-    $current_time = time();
-    $cache_time = $cache_ttl + $current_time;
-   
+
+
     if ($language == 'en') {
       $locale = 'row';
     }
@@ -36,22 +34,25 @@ class BrancottRestApiController extends ControllerBase {
     $request_type = 'wine_details';
     $cid = $request_type . '_' . $locale . '_' . $wine_id;
     $cache = \Drupal::cache()->get($cid);
-    
-	if ($cache) {
+
+    if ($cache) {
       $result = json_decode($cache->data);
       return $result;
     }
     else {
       $vc = $this->config('brancott_our_wines.settings');
       $dch_ranges_url = $vc->get('dch_wine_url');
-      $response = brancott_rest_api_reponse($dch_ranges_url. '/' . $locale . '/wines/' . $wine_id . '/en');
+      $response = brancott_rest_api_reponse($dch_ranges_url . '/' . $locale . '/wines/' . $wine_id . '/en');
     }
-	
+
     if ($response['code'] == 200) {
-		$result = json_decode($response['result']);
-		if($result->id) {
-			\Drupal::cache()->set($cid, $response['result'], $cache_time);
-		}
+      $cache_ttl = \Drupal::config('brancott_our_wines.settings')->get('cache_ttl');
+      $current_time = time();
+      $cache_time = $cache_ttl + $current_time;
+      $result = json_decode($response['result']);
+      if ($result->id) {
+        \Drupal::cache()->set($cid, $response['result'], $cache_time);
+      }
       $data = $result;
     }
     return $data;
