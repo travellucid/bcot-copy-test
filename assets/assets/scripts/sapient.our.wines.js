ffe80301a8fcceddec81886134ef185dbece08d1
Rohit Pandey 3 <rpan22@sapient.com>
2017-03-21 16:12:51 +0530

    ipad wine filtrs

--- assets/assets/scripts/sapient.our.wines.js
+++ assets/assets/scripts/sapient.our.wines.js
@@ -153,15 +153,23 @@ var ourWines = (function($, window, sapient) {
 				});
 			},
 			onClickOutside = function() {
-				$(document).on('touchend', '#hero-component, #product-grid',function(e) {
+				$(document).on('touchend', function(e) {
 					
+					if($(e.target).is("li")) {
+
+						$(".level-2.list-reset").css({"opacity":1, "left": 0 });
+
+						$(".hovered >a").css("color","#d50032");
+					}
+					else if ($(e.target).is('.wine-filters-desktop, .wine-filters-desktop *')) {
+						return;
+					}
 					$(".level-2.list-reset").css({"opacity":0, "left": -9999 });
 					$(".wine-filters-desktop .hovered").css("background-color","#1e7266");
 					$(".hovered >a").css("color","white");
 					$(".wine-filters-desktop .hovered ").removeClass("hovered");
 				});
 			}
-			;
 
 
 		return {
