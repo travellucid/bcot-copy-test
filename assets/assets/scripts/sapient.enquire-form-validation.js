645db07d055364832afa49b6afe379d219d55fd6
Rohit Pandey 3 <rpan22@sapient.com>
2017-04-10 15:48:51 +0530

    err undefined form

--- assets/assets/scripts/sapient.enquire-form-validation.js
+++ assets/assets/scripts/sapient.enquire-form-validation.js
@@ -26,7 +26,7 @@ var validationObj = (function($, window, sapient) {
 					$alphaOnly = $(".enquire-form .group .alpha-only").filter('[required]'),
 					
 					filterEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-					filterAlphaNumeric =  /[^a-z\d]/i,
+					filterAlphaNumeric = /^[a-z0-9]+$/i,
 					filterAlphaOnly = /^[a-zA-Z]*$/,
 					filterNumericOnly = /^\d+$/;	
 				
