0311c9cfe40e0a3f106395dd542d9c63ea3b19b4
Ayush Krishnatray <akri11@sapient.com>
2017-05-09 18:57:30 +0530

    alert added

--- assets/assets/scripts/sapient.common.js
+++ assets/assets/scripts/sapient.common.js
@@ -260,10 +260,10 @@ var commonObj = (function($, window, sapient) {
 						cookieFinal = val;
 					}
 					else{
-						if(cookieFinal != ''){
+
 							cookieFinal = 'en';
-						}
-					}
+						
+					    }
 					if(cookieFinal != urlFinal){
 						document.cookie = "age_checked"+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 						location.reload(true);
