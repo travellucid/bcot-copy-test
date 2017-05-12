b171e00b29fdd95ec8a92a7e2818465019a5009f
Sandeep Tanwar <stanw2@sapient.com>
2017-05-12 14:27:38 +0530

    test

--- docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
+++ docroot/themes/sapient_brancott/js/brancott_js/sapient.modules.ver.1.0.0.dev.js
@@ -620,15 +620,15 @@ var commonObj = (function($, window, sapient) {
 							cookieFinal = val;
 						}
 						else{
-							if(cookieFinal != '' && cookieFinal == 'fa'){
+							if(val != '' && val == 'fa'){
 								cookieFinal = urlFinal;
 							}
-							if(cookieFinal != ''){
+							if(val != '' && val != 'fa'){
 								cookieFinal = 'en';
 							}
 						}
 						alert(cookieFinal);
-						alert(urlFinal);
+						
 						if(cookieFinal != urlFinal){
 							document.cookie = "age_checked"+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 							location.reload(true);
